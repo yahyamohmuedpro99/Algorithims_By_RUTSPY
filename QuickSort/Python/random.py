27472f282c5ef76f0f1455a1138fc6efa43eb605
yahya mo <yahyamohmuedpro99@gmail.com>
2024-02-05 10:28:35 +0200

    add max using recursion

--- QuickSort/Python/random.py
+++ QuickSort/Python/random.py
@@ -1,4 +1,4 @@
-l=[1,2,3,4,6]
+l=[1,2,33]
 
 def sum(arr):
     arr=arr.copy()
@@ -19,4 +19,13 @@ def sum3(arr):
     else:
         return arr[-1]+sum(arr[:-1])
 
-print(sum3(l))
+def max(arr):
+    if len(arr) == 2:
+        return arr[0] if arr[0] > arr[1] else arr[1]
+    else:
+        sub_max = max(arr[1:])
+        #we take the arr[0] bcz we slice from arr[1:] which mean we have to back to first element 
+        return sub_max if sub_max > arr[0] else arr[0] 
+
+arr=[10,20]
+print(max(l))
